dd98194d65f25be3a2b9f5fbd59621794fccb77d
Stefanie Kielbasa <skielbasa@srkdesign.com>
2025-04-24 10:30:47 -0400

    Initial version of update view

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,11 +9,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+
+app.get('/update-cobj', async (req, res) => {
+
+  const objects = 'https://api.hubspot.com/crm/v3/objects/contacts';
+  const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+  }
+
+  try {
+      const resp = await axios.get(objects, { headers });
+      const data = resp.data.results;
+
+      res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });
+  } catch (error) {
+      console.error(error);
+  }
+
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
